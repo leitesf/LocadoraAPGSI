e066d41aa7f121abdf0102466a3911b5bce0b294
Rafael Leite <rafaelleitedefreitas@gmail.com>
2011-02-22 09:13:23 -0300

    Adicionado busca de copias em mal estado

--- app/models/copia.rb
+++ app/models/copia.rb
@@ -4,5 +4,10 @@ class Copia < ActiveRecord::Base
   has_many :emprestimos
   has_and_belongs_to_many :ators
   validates_presence_of :titulo, :duracao, :ano, :aquisicao, :estado
+
+  def self.copias_em_mal_estado
+    Copia.find(:all, :conditions => {:estado => 'Ruim'})
+  end
+
 end
 
--- spec/models/copia_spec.rb
+++ spec/models/copia_spec.rb
@@ -4,70 +4,81 @@ describe Copia do
 
 	fixtures :copias
 
-  it "deveria ser válido" do
-    lambda {
-			copia = create_ator
+  context 'Deveria validar o model de Copia: ' do
+    it "deveria ser válido" do
+      lambda {
+        copia = create_ator
 		}
+    end
+    
+    it 'deveria buscar a lista de copias em mal estado' do
+      copias = Copia.copias_em_mal_estado
+      for copia in copias
+        copia.estado.should eql('Ruim')
+      end
+    end
+
+    it "deveria ser invalido sem titulo" do
+      lambda {
+        copia = create_copia(:titulo => nil)
+        copia.errors.should be_invalid(:titulo)
+      }.should_not change(Copia, :count)
+    end
+
+    it "deveria ser invalido sem duracao" do
+      lambda {
+        copia = create_copia(:duracao => nil)
+        copia.errors.should be_invalid(:duracao)
+      }.should_not change(Copia, :count)
+    end
+
+    it "deveria ser invalido sem ano" do
+      lambda {
+        copia = create_copia(:ano => nil)
+        copia.errors.should be_invalid(:ano)
+      }.should_not change(Copia, :count)
+    end
+
+    it "deveria ser invalido sem data de aquisição" do
+      lambda {
+        copia = create_copia(:aquisicao => nil)
+        copia.errors.should be_invalid(:aquisicao)
+      }.should_not change(Copia, :count)
+    end
+
+    it "deveria ser invalido sem estado" do
+      lambda {
+        copia = create_copia(:estado => nil)
+        copia.errors.should be_invalid(:estado)
+      }.should_not change(Copia, :count)
+    end
+
+    it "deveria possuir relacionamento com atores" do
+      lambda {
+        copias(:nao_foram).ators
+      }.should_not raise_error
+    end
+
+    it "deveria possuir relacionamento com diretor" do
+      lambda {
+        copias(:nao_foram).diretor
+      }.should_not raise_error
+    end
+
+    it "deveria possuir relacionamento com genero" do
+      lambda {
+        copias(:nao_foram).genero
+      }.should_not raise_error
+    end
+
+    it "deveria possuir relacionamento com emprestimos" do
+      lambda {
+        copias(:nao_foram).emprestimos
+      }.should_not raise_error
+    end
   end
 
-	it "deveria ser invalido sem titulo" do
-		lambda {
-			copia = create_copia(:titulo => nil)
-			copia.errors.should be_invalid(:titulo)
-		}.should_not change(Copia, :count)
-	end
-
-	it "deveria ser invalido sem duracao" do
-		lambda {
-			copia = create_copia(:duracao => nil)
-			copia.errors.should be_invalid(:duracao)
-		}.should_not change(Copia, :count)
-	end
-
-	it "deveria ser invalido sem ano" do
-		lambda {
-			copia = create_copia(:ano => nil)
-			copia.errors.should be_invalid(:ano)
-		}.should_not change(Copia, :count)
-	end
-
-	it "deveria ser invalido sem data de aquisição" do
-		lambda {
-			copia = create_copia(:aquisicao => nil)
-			copia.errors.should be_invalid(:aquisicao)
-		}.should_not change(Copia, :count)
-	end
-
-	it "deveria ser invalido sem estado" do
-		lambda {
-			copia = create_copia(:estado => nil)
-			copia.errors.should be_invalid(:estado)
-		}.should_not change(Copia, :count)
-	end
-
-	it "deveria possuir relacionamento com atores" do
-		lambda {
-			copias(:nao_foram).ators
-		}.should_not raise_error
-	end
-
-	it "deveria possuir relacionamento com diretor" do
-		lambda {
-			copias(:nao_foram).diretor
-		}.should_not raise_error
-	end
-
-	it "deveria possuir relacionamento com genero" do
-		lambda {
-			copias(:nao_foram).genero
-		}.should_not raise_error
-	end
-
-	it "deveria possuir relacionamento com emprestimos" do
-		lambda {
-			copias(:nao_foram).emprestimos
-		}.should_not raise_error
-	end
+  
 
 	private
     def create_copia(options={})
