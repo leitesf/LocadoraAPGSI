2b6d2a8c43a5fe9ae619fe7bc6aa8c5a3e807e32
Rafael Leite de Freitas <rafaelleitedefreitas@gmail.com>
2011-02-25 09:55:32 -0300

    atualizando model Filme para buscar filmes por titulo

--- app/models/filme.rb
+++ app/models/filme.rb
@@ -5,5 +5,9 @@ class Filme < ActiveRecord::Base
   has_many :copias_livres, :class_name => "Copia", :conditions => {:locado => 'false'}
   has_and_belongs_to_many :ators
   validates_presence_of :titulo, :duracao, :ano
+
+  def self.buscar_por_titulo(titulo)
+    self.find(:all, :conditions => "titulo like '%#{titulo}%'")
+  end
 end
 
