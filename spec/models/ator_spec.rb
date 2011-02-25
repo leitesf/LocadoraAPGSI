88c0fc29ad4d38a9898b537aa4013cba233972d4
Rafael Leite de Freitas <rafaelleitedefreitas@gmail.com>
2011-02-25 10:40:24 -0300

    adicionado teste para busca de filmes por ator

--- spec/models/ator_spec.rb
+++ spec/models/ator_spec.rb
@@ -2,41 +2,52 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Ator do
 
-	fixtures :ators
+	fixtures :ators, :filmes
 
-  it "deveria ser válido" do
-    lambda {
-			ator = create_ator
-		}
+  context 'Deveria validar o model de Ator: ' do
+
+    it "deveria ser válido" do
+      lambda {
+        ator = create_ator
+      }
+    end
+
+    it "deveria ser invalido sem nome" do
+      lambda {
+        ator = create_ator(:nome => nil)
+        ator.errors.should be_invalid(:nome)
+      }.should_not change(Ator, :count)
+    end
+
+    it "deveria ser invalido sem nacionalidade" do
+      lambda {
+        ator = create_ator(:nacionalidade => nil)
+        ator.errors.should be_invalid(:nacionalidade)
+      }.should_not change(Ator, :count)
+    end
+
+    it "deveria ser invalido sem data_nascimento" do
+      lambda {
+        ator = create_ator(:data_nascimento => nil)
+        ator.errors.should be_invalid(:data_nascimento)
+      }.should_not change(Ator, :count)
+    end
+
+    it "deveria possuir relacionamento com filmes" do
+      lambda {
+        ators(:hanks).filmes
+      }.should_not raise_error
+    end
+
+    it "deveria buscar filmes por ator" do
+      ator = Ator.buscar_por_nome('Tom Hanks')
+      for filme in ator.filmes
+        filme.ator.nome.should eql('Tom Hanks')
+      end
+    end
+
   end
-
-	it "deveria ser invalido sem nome" do
-		lambda {
-			ator = create_ator(:nome => nil)
-			ator.errors.should be_invalid(:nome)
-		}.should_not change(Ator, :count)
-	end
-
-	it "deveria ser invalido sem nacionalidade" do
-		lambda {
-			ator = create_ator(:nacionalidade => nil)
-			ator.errors.should be_invalid(:nacionalidade)
-		}.should_not change(Ator, :count)
-	end
-
-	it "deveria ser invalido sem data_nascimento" do
-		lambda {
-			ator = create_ator(:data_nascimento => nil)
-			ator.errors.should be_invalid(:data_nascimento)
-		}.should_not change(Ator, :count)
-	end
-
-	it "deveria possuir relacionamento com filmes" do
-		lambda {
-			ators(:hanks).filmes
-		}.should_not raise_error
-	end
-
+  
 	private
     def create_ator(options={})
       Ator.create({
