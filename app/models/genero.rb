3e8402cbfeb1eba9c8a221b16617c2d5bdc1834c
Rafael Leite de Freitas <rafaelleitedefreitas@gmail.com>
2011-02-25 10:35:24 -0300

    adequando model Genero para atender ao teste de busca de filmes por genero

--- app/models/genero.rb
+++ app/models/genero.rb
@@ -1,5 +1,9 @@
 class Genero < ActiveRecord::Base
   has_many :filmes
   validates_presence_of :descricao
+
+  def self.buscar_por_descricao(descricao)
+    self.find(:first, :conditions => "descricao like '%#{descricao}%'")
+  end
 end
 
