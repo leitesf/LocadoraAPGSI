c754c9a4127d01ecf92f5836898eb9010657bbb9
Rafael Leite de Freitas <rafaelleitedefreitas@gmail.com>
2011-03-15 09:03:02 -0300

    Corrigindo o model de ator que não havia sido commitado corretamente

--- app/models/ator.rb
+++ app/models/ator.rb
@@ -1,5 +1,9 @@
 class Ator < ActiveRecord::Base
   has_and_belongs_to_many :filmes
   validates_presence_of :nome, :data_nascimento, :nacionalidade
+
+  def self.buscar_por_nome(nome)
+    self.find(:first, :conditions => "nome like '%#{nome}%'")
+  end
 end
 
