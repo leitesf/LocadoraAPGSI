c2c5df376e95d44ce91a67580b324cd4d7487ef4
Rafael Leite de Freitas <rafaelleitedefreitas@gmail.com>
2011-03-17 09:59:28 -0300

    Adicionando a interface do sócio, que inclui a função de empréstimo

---
+++ app/controllers/socios_controller.rb
@@ -0,0 +1,64 @@
+class SociosController < ApplicationController
+  def index
+    @socios = Socio.all
+  end
+
+  def show
+    @socio = Socio.find(params[:id])
+    @ators = Ator.all.collect{|p| [p.nome, p.id]}
+  end
+
+  def new
+    @socio = Socio.new
+    @diretores = Diretor.all.collect{|p| [p.nome, p.id]}
+    @generos = Genero.all.collect{|p| [p.descricao, p.id]}
+  end
+
+  def new_emprestimo
+    @socio = Socio.find(params[:socio_id])
+    @copia = Copia.find(params[:emprestimo][:copia_id])
+    unless @copia.locado
+      @emprestimo = @copia.emprestar(@socio)
+      render :update do |p|
+        p.insert_html :bottom, 'list_emprestimos_devidos', :partial => 'item_list_emprestimo', :locals => {:emprestimo => @emprestimo}
+      end
+    else
+      render :update do |p|
+        p.alert 'Esta cópia está indisponível'
+      end
+    end
+  end
+
+  def edit
+    @socio = Socio.find(params[:id])
+    @diretores = Diretor.all.collect{|p| [p.nome, p.id]}
+    @generos = Genero.all.collect{|p| [p.descricao, p.id]}
+  end
+
+  def create
+    @socio = Socio.new(params[:socio])
+    if @socio.save
+        flash[:notice] = 'O socio foi criado com sucesso.'
+        redirect_to(@socio)
+    else
+        render :action => "new"
+    end
+  end
+
+  def update
+    @socio = Socio.find(params[:id])
+      if @socio.update_attributes(params[:socio])
+        flash[:notice] = 'O socio foi atualizado com sucesso.'
+        redirect_to(@socio)
+      else
+        render :action => "edit"
+      end
+  end
+
+  def destroy
+    @socio = Socio.find(params[:id])
+    @socio.destroy
+    redirect_to(socios_url)
+  end
+
+end
--- app/models/socio.rb
+++ app/models/socio.rb
@@ -2,5 +2,9 @@ class Socio < ActiveRecord::Base
   has_many :emprestimos
   has_many :emprestimos_devidos, :class_name => "Emprestimo", :conditions => {:data_devolucao => nil}
   validates_presence_of :nome, :endereco, :telefone
+
+  def emprestimos_devolvidos
+    self.emprestimos - self.emprestimos_devidos
+  end
 end
 
